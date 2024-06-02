375d1d1fc180b47f672a8545dda34aca4a21d8ba
Adrian Toică <64870406+Skipzs@users.noreply.github.com>
2024-06-02 16:20:01 +0300

    Edit

--- bk/Domain/obj/Debug/net6.0/Domain.AssemblyInfo.cs
+++ bk/Domain/obj/Debug/net6.0/Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b61fcfa5837587cda8d61171ff98a75528c7f2c2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8f9f1e01c83d2fb529da11f5495aebce4a8c9530")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
